cbae51135dd34b7ef72f44c464bea9f7d39f7eec
stdimitrov05 <stdimitrov8b@gmail.com>
2021-05-19 14:03:06 +0300

    update

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ io.on('connection', socket => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
